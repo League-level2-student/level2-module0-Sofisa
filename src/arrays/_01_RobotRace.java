5656c6ba639f3170718773eef0caba523cefad8e
Samuel Sharp <league@wts7.attlocal.net>
2018-08-12 17:32:12 -0700

    Robot Race, hidden buttons(IP)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,5 +1,9 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
@@ -9,16 +13,50 @@ public class _01_RobotRace {
 	
 	//2. create an array of 5 robots.
 	Robot[] robots = new Robot[5];
+	Random randy = new Random();
 	//3. use a for loop to initialize the robots.
+	//while(robots[5].getY() > 0) {	
 	for (int i = 0; i < robots.length; i++) {
 		robots[i] = new Robot();
-		robots[i].setX(0);
+		robots[i].miniaturize();
+		robots[i].setX(650);
+		robots[i].setY(300);
+		robots[i].setSpeed(randy.nextInt(90));
+		robots[i].getY();
+		
+
 	}
+	
+	
+		
+	for (int j = 0; j < 2100; j++) {
+		
+		
+	for (int k = 0; k < robots.length; k++) {
+		int random = randy.nextInt(10);
+		
+		if (random >= 9) {
+			
+		
+		robots[k].move(20);
+		robots[k].turn(-5);
+		robots[k].getX();
+	}
+	
+	if(robots[k].getX() > 645 && j > 70) {
+		JOptionPane.showMessageDialog(null, "WINNER");
+	}
+	}		
+	}	
+		
+	
+	
+	
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 	
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+	
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
