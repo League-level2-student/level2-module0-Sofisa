89e0e7df0fab7e3b2ce3ecf0ff45bcf51c29da99
ArnavPalkhiwala <league@WTS10.attlocal.net>
2018-08-26 17:26:54 -0700

    HiddenButtons, VisualArraySorter (IP)

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -21,11 +21,13 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	
 	//1. create an array of JButtons. Don't initialize it yet.
 	JButton[] buttons; 
+	Random randy = new Random();
 	//2 create an int variable called hiddenButton
-	int hiddenButton = 2;
+	int hiddenButton = 0;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
+	
 	
 	public void start() {
 		window = new JFrame("Find the Button");
@@ -37,7 +39,7 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		int number = Integer.parseInt(question);
 		
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		JButton[] buttons = new JButton[number];
+		buttons = new JButton[number];
 		
 		//5. Make a for loop to iterate through the JButton array
 		for (int i = 0; i < buttons.length; i++) {
@@ -45,6 +47,7 @@ public class _02_FindTheHiddenButton implements ActionListener{
 			buttons[i].addActionListener(this);
 			panel.add(buttons[i]);
 		}
+		
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
@@ -57,22 +60,33 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		//11. set the JFrame to visible.
 		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		JOptionPane.showMessageDialog(null, "");
+		JOptionPane.showMessageDialog(null, "Try to find the hidden button!");
 		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
+		hiddenButton = randy.nextInt(number);
 		
 		//14. Set the text of the JButton located at hiddenButton the read "ME"
-
+		buttons[hiddenButton].setText("ME");
 		//15. Use Thread.sleep(100); to pause the program.
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//    Surround it with a try/catch
 		
 		//16. Set the text of the JButton located at hiddenButton to be blank.
-		
+		buttons[hiddenButton].setText(" ");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonClicked = (JButton)e.getSource();
-		
+		if (buttons[hiddenButton] == buttonClicked) {
+			JOptionPane.showMessageDialog(null, "You Win!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Try again");
+		}
 		//17. if the hiddenButton is clicked, tell the user that they win.
 		
 		//18. else tell them to try again
--- visualArraySorter/visualArraySorter.pde
+++ visualArraySorter/visualArraySorter.pde
@@ -1,41 +1,53 @@
-/* Paste the code into a new Processing sketch and complete the steps. */
-
 //1. create an array of ints. don't initialize it yet.
-int[] number;
-void setup() {
-  //2. set the size of your window
-  //  if you are using Processing 3, you cannot use variables
-  //  because the developers don't know how programming is supposed to work.
+int[] numbers;
 
-  //3. initialize your array with the built in width variable
+void setup() {
   
+  //2. set the size of your window
+  size(400,400);
+  //  if you are using Processing 3, you cannot use variables
+  
+  //  because the developers don't know how programming is supposed to work.
+  
+  //3. initialize your array with the built in width variable
+  numbers = new int[width];
   //4. initialize the ints in the array with random numbers
+
   //   from 0 to the built in height variable
 
   //5. call the noStroke() method
-
+  noStroke();
 }
 
 void draw() {
+  
   //6. set the background color with background(r, g, b);
-
+  background(212,222,252);
   //7. set the color for your graph
-
+  fill(252,229,212);
   //8. draw a rectangle for each int in your array.
+  rect(numbers,height,1,1);
   //   the x value will be the element of the int
+  
   //   the y value will the height variable
+  
   //   the width is 1
+  
   //   the height is negative the value of the int at that element in the array
 
 
   //9. call the stepSort method
-
+  stepSort(int[] arr);
   //10. extract the code that randomizes the array into a method.
 
   //11. call the method you made in step 10 when the mouse is pressed
 
 }
-
+void randomm(){
+  for(int j=0; j<numbers.length; j++){
+   numbers[j] = int(random(height)) ; 
+  }
+}
 void stepSort(int[] arr) {
   for (int i = 1; i < arr.length; i++) {
     if (arr[i - 1] > arr[i]) {
@@ -44,4 +56,5 @@ void stepSort(int[] arr) {
       arr[i - 1] = t;
     }
   }
+
 }
